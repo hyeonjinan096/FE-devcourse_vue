1bd82b0a298c2a381fc7bcfa4c241f1e53390bb2
hyeonjinan <hyeonjinan096@naver.com>
2023-11-24 15:27:07 +0900

    'Create_project'

---
+++ src/components/Hello.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>Hello</h1>
+</template>
+
+<style scoped lang="scss">
+$color: orange;
+h1 {
+    color: $color
+}
+</style>
+
+<!-- 
+scoped: 하위 컴포넌트의 스타일이 상위컴포넌트에 영향을 미치는 것을 방지한다.  
+해당 컴포넌트에만 스타일 해쉬가 추가된다.
+-->
