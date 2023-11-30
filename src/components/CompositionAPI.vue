9a255e1765522ab9b1aa2bab818ea75646224d1a
hyeonjinan <hyeonjinan096@naver.com>
2023-11-30 15:22:42 +0900

    Composition_vs_Options(ref,method,lifecycle)

---
+++ src/components/CompositionAPI.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+const number = ref(0);
+console.log('Created!')
+onMounted(()=> {
+    console.log('Mounted')
+})
+
+
+function increase() {
+  number.value += 1;
+}
+</script>
+
+<template>
+  <h1 @click ="increase">{{ number }}</h1>
+</template>
---
+++ src/components/OptionsAPI.vue
@@ -0,0 +1,26 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+export default defineComponent({
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  created() {
+    console.log('Created!')
+  },
+  mounted() {
+    console.log('Mounted!')
+  },
+  methods: {
+    increase() {
+        this.count+=1
+    }
+  }
+  }
+);
+</script>
+
+<template>
+  <h1>{{ count }}</h1>
+</template>
