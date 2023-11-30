9cd689a50ae873db7fe77e8198efe1b736d0e04d
hyeonjinan <hyeonjinan096@naver.com>
2023-11-30 17:11:57 +0900

    Composition_vs_Options(ref_focus)

--- src/components/CompositionAPI.vue
+++ src/components/CompositionAPI.vue
@@ -1,24 +1,16 @@
 <script setup lang="ts">
-import { ref, reactive, watch } from 'vue';
-//반응형 데이터 만들기 2두가지 방법 : ref, reactive
-// reactive는 배열, 객체와 같은 참조형 데이터만 가능하다.
-// const userName = ref('Heropy');
-const user = reactive({
-  name: 'Heropy',
-  age: 85,
-});
+import { ref, onMounted } from 'vue';
 
-watch(
-  user,
-  (newVal) => {
-    console.log(newVal);
-  },
-);
 
-console.log(user.name, user.age);
+const inputEl = ref<HTMLInputElement | null>(null)
+
+onMounted(()=>{
+  if(inputEl.value) {
+    inputEl.value.focus()
+  }
+})
 </script>
 
 <template>
-  <input v-model="user.name" />
-  <h1>{{ user.name }}</h1>
+  <input ref="inputEl" />
 </template>
--- src/components/OptionsAPI.vue
+++ src/components/OptionsAPI.vue
@@ -1,42 +1,14 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 export default defineComponent({
-  data() {
-    return {
-      number: 0,
-    };
-  },
-  computed: {
-    double : {
-      //Getter
-      get() {
-        return this.number * 2;
-      },
-      //Setter
-      set(newVal:number) {
-        this.number = newVal / 2
-      }
-    },
-  },
-  watch: {
-    count(newVal, oldVal){
-      console.log('number: ', newVal, oldVal)
+  mounted() {
+    if (this.$refs.inputEl) {
+      (this.$refs.inputEl as HTMLInputElement).focus();
     }
   },
-  methods: {
-    increase() {
-      this.number+=1
-    },
-    assign() {
-      this.double = 0
-    }
-  }
 });
 </script>
 
 <template>
-  <button @click="increase">Increase</button>
-  <button @click="assign">Assign</button>
-  <h1>{{ number }}</h1>
-  <h2>{{ double }}</h2>
+  <input ref="inputEl" />
 </template>
